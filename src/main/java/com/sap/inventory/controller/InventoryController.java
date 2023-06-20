a444585efbaa87b56b8dab4d4efac69d2f6ffe2b
Niranjan <niranjan.b@in.pradeepit.com>
2023-06-20 15:27:03 +0530

    Search api

--- src/main/java/com/sap/inventory/controller/InventoryController.java
+++ src/main/java/com/sap/inventory/controller/InventoryController.java
@@ -5,6 +5,7 @@ import com.sap.inventory.service.InventoryService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/inventory")
+@Validated
 public class InventoryController {
 
     private final InventoryService inventoryService;
